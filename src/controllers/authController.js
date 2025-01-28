33ccf0efe0ee81756b61a41175d431404c17506f
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-01-28 13:53:23 +0700

    fixing bug payment gateway

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,11 @@
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
-import TransacttionModel from "../models/transactionModel.js";
+import transactionModel from "../models/transactionModel.js";
 
 export const signUpAction = async (req, res) => {
   const midtransUrl = process.env.MIDTRANS_URL;
   const midtransAuthString = process.env.MIDTRANS_AUTH_STRING;
-  //   const pathFinishPayment = process.env.PATH_FINISH_PAYMENT;
+  const pathFinishPayment = process.env.PATH_FINISH_PAYMENT;
 
   try {
     const body = req.body; //name, email, password
@@ -21,17 +21,13 @@ export const signUpAction = async (req, res) => {
     });
 
     // action payment gateway midtrans
-    const transaction = new TransacttionModel({
+    const transaction = new transactionModel({
       user: user._id,
       price: 280000,
     });
 
     const midtrans = await fetch(midtransUrl, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${midtransAuthString}`,
-      },
       body: JSON.stringify({
         transaction_details: {
           order_id: transaction._id.toString(),
@@ -44,15 +40,21 @@ export const signUpAction = async (req, res) => {
           email: user.email,
         },
         callbacks: {
-          finish: "http://localhost:5173/success-checkout",
+          finish: pathFinishPayment,
         },
       }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${midtransAuthString}`,
+      },
     });
 
     const resMidtrans = await midtrans.json();
 
     await user.save();
     await transaction.save();
+
+    console.log(resMidtrans);
 
     return res.json({
       message: "Sign Up Success",
