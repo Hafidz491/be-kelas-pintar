6d2b4fb0c25392362b672800248d44a489852074
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-01-28 15:04:59 +0700

    create handle payment controller

---
+++ src/controllers/paymentController.js
@@ -0,0 +1,36 @@
+import transactionModel from "../models/transactionModel.js";
+
+export const paymentController = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const objectId = body.order_id;
+
+    switch (body.transaction_status) {
+      case "capture":
+      case "settlement":
+        await transactionModel.findByIdAndUpdate(objectId, {
+          status: "success",
+        });
+        break;
+      case "deny":
+      case "cancel":
+      case "expire":
+      case "pending":
+      case "failure":
+        await transactionModel.findByIdAndUpdate(objectId, {
+          status: "failed",
+        });
+        break;
+    }
+
+    return res.json({
+      message: "Payment Success!",
+      data: {},
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
