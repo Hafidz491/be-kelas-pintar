eb0d7ce8782529cb8e5b81aab187411b71e2b955
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-02-04 00:03:36 +0700

    create sign in action controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import transactionModel from "../models/transactionModel.js";
+import jwt from "jsonwebtoken";
 
 export const signUpAction = async (req, res) => {
   const midtransUrl = process.env.MIDTRANS_URL;
@@ -58,6 +59,60 @@ export const signUpAction = async (req, res) => {
       message: "Sign Up Success",
       data: {
         midtrans_payment_url: resMidtrans.redirect_url,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
+
+export const signInAction = async (req, res) => {
+  try {
+    const body = req.body; //email, password
+
+    const existingUser = await userModel
+      .findOne()
+      .where("email")
+      .equals(body.email);
+
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const comparePassword = bcrypt.compareSync(
+      body.password,
+      existingUser.password
+    );
+
+    if (!comparePassword) {
+      return res.status(401).json({ error: "Email or Password incorrect!" });
+    }
+
+    const isValidUser = await transactionModel.findOne({
+      user: existingUser._id,
+      status: "success",
+    });
+
+    if (existingUser.role !== "student" && !isValidUser) {
+      return res.status(400).json({ error: "User not verified!" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id.toString(),
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1 day" }
+    );
+
+    return res.json({
+      message: "Sign in success",
+      data: {
+        name: existingUser.name,
+        email: existingUser.email,
+        token,
+        role: existingUser.role,
       },
     });
   } catch (error) {
