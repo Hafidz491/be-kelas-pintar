7645f62e1266a3fd33f934eaae15982f7eee8f8b
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-02-14 01:34:50 +0700

    fix bug to get data course

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -4,16 +4,17 @@ import userModel from "../models/userModel.js";
 const verifyToken = async (req, res, next) => {
   const secretKey = process.env.JWT_SECRET_KEY ?? "";
 
-  if (req?.headers?.authorization?.split(" ")[0] === "Bearer") {
+  if (req?.headers?.authorization?.split(" ")[0] === "JWT") {
     const decoded = jwt.verify(
       req?.headers?.authorization?.split(" ")[1],
       secretKey
     );
 
-    const user = await userModel.findById(
-      decoded.data.id,
-      "_id name email role"
-    );
+    const user = await userModel.findById(decoded.id, "_id name email role");
+
+    if (!decoded?.id) {
+      return res.status(400).json({ message: "Invalid Token Structure" });
+    }
 
     if (!user) {
       return res.status(400).json({ message: "Token expired" });
@@ -28,7 +29,7 @@ const verifyToken = async (req, res, next) => {
 
     next();
   } else {
-    return res.status(400).json({ message: "Unauthorized" });
+    return res.status(400).json({ message: "Unauthorization" });
   }
 };
 
