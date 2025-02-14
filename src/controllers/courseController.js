1d36333bd875e769f76c2bc4a8f6a4acf4c812dd
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-02-14 22:13:00 +0700

    create postCourse controller

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,4 +1,7 @@
 import courseModel from "../models/courseModel.js";
+import { mutateCourseSchema } from "../utils/schema.js";
+import categoryModel from "../models/categoryModel.js";
+import userModel from "../models/userModel.js";
 
 export const getCourses = async (req, res) => {
   try {
@@ -17,5 +20,67 @@ export const getCourses = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Internal Server Error!" });
+  }
+};
+
+export const postCourse = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const parse = mutateCourseSchema.safeParse(body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map((err) => err.message);
+
+      if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+        fs.unlinkSync(req?.file?.path);
+      }
+
+      return res.status(500).json({
+        message: "Invalid Request",
+        data: null,
+        errors: errorMessages,
+      });
+    }
+
+    const category = await categoryModel.findById(parse.data.categoryId);
+
+    if (!category) {
+      return res.status(500).json({
+        message: "Category Id not found",
+      });
+    }
+
+    const course = new courseModel({
+      name: parse.data.name,
+      thumbnail: req.file?.filename,
+      category: category._id,
+      tagline: parse.data.tagline,
+      description: parse.data.description,
+      manager: req.user._id,
+    });
+
+    await course.save();
+
+    await categoryModel.findByIdAndUpdate(
+      category._id,
+      {
+        $push: { courses: course._id },
+      },
+      { new: true }
+    );
+
+    await userModel.findByIdAndUpdate(
+      req.user?._id,
+      {
+        $push: { courses: course._id },
+      },
+      { new: true }
+    );
+
+    return res.json({ message: "Create course success" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error!" });
   }
 };
